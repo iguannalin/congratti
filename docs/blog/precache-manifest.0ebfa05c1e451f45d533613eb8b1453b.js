1bb36c378500f046c0bb51a95f84a91ee3bde1e2
seasons986 <annalin12345@gmail.com>
2020-05-10 19:24:54 -0700

    Revert "Updates"
    This reverts commit eb2afa6c3459909eb3c8f96392658b7be7e45c25.

---
+++ docs/blog/precache-manifest.0ebfa05c1e451f45d533613eb8b1453b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3456f3cf041a354fdcef9b9616af17b3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "af565e5c66a93a0679cd",
+    "url": "./static/css/main.ea4d3f68.chunk.css"
+  },
+  {
+    "revision": "58005315358346d6e2d8",
+    "url": "./static/js/2.f3211003.chunk.js"
+  },
+  {
+    "revision": "af565e5c66a93a0679cd",
+    "url": "./static/js/main.5680fc51.chunk.js"
+  },
+  {
+    "revision": "c3dc33dd3c0251b7173c",
+    "url": "./static/js/runtime-main.baab6bc7.js"
+  }
+]);
