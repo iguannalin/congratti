4c79b85d64e2059b27ca2aecd30ef690e630f497
seasons986 <annalin12345@gmail.com>
2020-05-10 22:05:22 -0700

    New build/ for picblog

--- docs/picblog/build/service-worker.js
+++ docs/picblog/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e25972e05eda201103c20ffeccd5fff.js"
+  "./precache-manifest.7c75d4c74bfc581b3eccff42a32d6ff8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
