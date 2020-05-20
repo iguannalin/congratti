f0ac56689cac74291e54cfa2ccd27125e2bba7f0
seasons986 <annalin12345@gmail.com>
2020-05-20 01:37:10 -0700

    New auto-build of portfolio, ready to deploy

---
+++ docs/portfolio/build/precache-manifest.708d0f3afd5d62368b9f1980f9b1ea0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a87308b94664affa48cafaac150e6f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "31f3cc90739be977fe75",
+    "url": "./static/css/main.12a76d9d.chunk.css"
+  },
+  {
+    "revision": "259a3dd26e3a56a15ee5",
+    "url": "./static/js/2.ff8fd642.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ff8fd642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31f3cc90739be977fe75",
+    "url": "./static/js/main.ae614877.chunk.js"
+  },
+  {
+    "revision": "85856eca26956ea128b4",
+    "url": "./static/js/runtime-main.51f1d894.js"
+  }
+]);
