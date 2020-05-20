59591287aa80a46182c85480a319984f776176ab
seasons986 <annalin12345@gmail.com>
2020-05-20 01:09:16 -0700

    Updating script

--- docs/deployportfolio.sh
+++ docs/deployportfolio.sh
@@ -1,6 +1,6 @@
 rm -rf docs/portfolio/
 git fetch origin portfolio
-git checkout portfolio -- docs/portfolio/
+git checkout portfolio -- portfolio-app/build
 git st
 git add docs/portfolio/
 git cm "New auto-build of portfolio, ready to deploy"
