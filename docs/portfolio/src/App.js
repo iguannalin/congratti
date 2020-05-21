43718ebdd626e7f94eea271f7d9b35b959384d98
seasons986 <annalin12345@gmail.com>
2020-05-21 00:33:50 -0700

    New portfolio look

--- docs/portfolio/src/App.js
+++ docs/portfolio/src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello, welcome to this work in progress</h1>
       <ul>
-        <h2>Projects:</h2>
+        <h1>Projects:</h1>
         <li><a href="https://iguannalin.github.io/eaturveg/">eaturveg</a></li>
+        <li><a href="https://iguannalin.github.io/spotify-recently-added/">Spotify Recently Added</a></li>
       </ul>
     </div>
   );
