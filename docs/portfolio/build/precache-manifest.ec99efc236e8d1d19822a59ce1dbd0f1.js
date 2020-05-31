dbae4c7cda8bc686a62d498aa5e205bd55787104
seasons986 <annalin12345@gmail.com>
2020-05-31 12:20:09 -0700

    Auto-generating portfolio

---
+++ docs/portfolio/build/precache-manifest.ec99efc236e8d1d19822a59ce1dbd0f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa8764b0db17b439b88620bed24c47f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cbb953c3c252ee08aa19",
+    "url": "./static/css/main.bfbfd3b1.chunk.css"
+  },
+  {
+    "revision": "259a3dd26e3a56a15ee5",
+    "url": "./static/js/2.ff8fd642.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ff8fd642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbb953c3c252ee08aa19",
+    "url": "./static/js/main.de5918df.chunk.js"
+  },
+  {
+    "revision": "85856eca26956ea128b4",
+    "url": "./static/js/runtime-main.51f1d894.js"
+  }
+]);
---
+++ docs/portfolio/runbuild.sh
@@ -0,0 +1,5 @@
+npm run build
+git st
+git add build
+git cm "Auto-committing new build"
+echo "Ready to push!"
