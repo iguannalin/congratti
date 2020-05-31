28b60409328044eefd7fde0c558360acd8aa07b0
seasons986 <annalin12345@gmail.com>
2020-05-31 12:57:06 -0700

    New look who dis

---
+++ docs/dev-portfolio/runbuild.sh
@@ -0,0 +1,5 @@
+npm run build
+git st
+git add ../portfolio
+git cm "Auto-committing portfolio"
+echo "Ready to push!"
---
+++ docs/dev-portfolio/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        <h1>Projects:</h1>
+        <li><a href="https://iguannalin.github.io/eaturveg/">eaturveg</a></li>
+        <li><a href="https://iguannalin.github.io/spotify-recently-added/">Spotify Recently Added</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
