329320adce56ab2dd80fdc718f48042704d5d279
seasons986 <annalin12345@gmail.com>
2020-06-09 01:49:55 -0700

    Auto-committing portfolio

--- docs/portfolio/service-worker.js
+++ docs/portfolio/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ed24372b9b95698faf9e314f0097734d.js"
+  "./precache-manifest.d6dc3f4e7de2fb2ccabfebf914b010d6.js"
 );
 
 self.addEventListener('message', (event) => {
