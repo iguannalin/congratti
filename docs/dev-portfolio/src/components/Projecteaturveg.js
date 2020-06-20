7a871cc1c9a0bb9a192c3ca4a325492b5d1b21a2
seasons986 <annalin12345@gmail.com>
2020-06-20 00:15:46 -0700

    Fixing eat ur veg link and adding details

--- docs/dev-portfolio/src/components/Projecteaturveg.js
+++ docs/dev-portfolio/src/components/Projecteaturveg.js
@@ -3,7 +3,15 @@ import React from "react";
 const Projecteaturveg = () => {
     return (
         <div>
-            <a href="https://iguannalin.github.io/eaturveg/Projecteaturveg.js">View Project</a>
+            <div className="project-link"><a href="https://iguannalin.github.io/eaturveg/">View Project</a></div>
+            <h2>About this project</h2>
+            <div className="project-details">
+                <p>I started this project at a time when I was kindling a love for both vegetables and JSON data.<br/>
+                    Unable to find a good source of information on vegetables that are in-season, I decided to scrape
+                    this <a
+                        href="https://github.com/iguannalin/eaturveg/blob/master/src/beautifulveggiesdata.json">data</a> from
+                    the local CUESA website.</p>
+            </div>
         </div>
     );
 };
