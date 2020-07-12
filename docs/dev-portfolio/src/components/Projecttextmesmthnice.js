10e709eaa058e79eb262e307c225aba831725c78
seasons986 <annalin12345@gmail.com>
2020-07-12 01:38:03 -0700

    Added a looong description for textmesmthnice

--- docs/dev-portfolio/src/components/Projecttextmesmthnice.js
+++ docs/dev-portfolio/src/components/Projecttextmesmthnice.js
@@ -7,7 +7,17 @@ const Projecttextmesmthnice = () => {
             <h2>About this project</h2>
             <div className="project-details">
                 <p>Currently trying to figure out how to use the Twilio sms api to take a list of phone numbers, and
-                    send out routine nice messages via a strange combination of Google App Engine, Heroku, and Python.</p>
+                    send out routine nice messages via a strange combination of Google App Engine, Heroku, and
+                    Python.</p>
+                <p>The way it currently works is: someone enters their phone number into HTML form -> submitting form
+                    makes a request to a Python app hosted by Google App Engine -> Python app writes the new phone
+                    number to a file in an online repository and pushes it -> Python app hosted by Heroku automatically
+                    redeploys on every push and runs script periodically to send texts to the list of phone numbers.</p>
+                <p>The hardest part was figuring out of all the technology out there, which ones to use for my use case
+                    and which were overkill. I think there's probably a way to do this in Django, to host a database
+                    that gets updated, instead of the combination of scripting a push in my app on Google App Engine,
+                    and waiting for that push on my app hosted on Heroku, and I'd love to revisit this some more. Also
+                    please feel free to write to me if you are reading this and have some thoughts.</p>
             </div>
         </div>
     );
