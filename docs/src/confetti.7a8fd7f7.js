081d7570746afb0750504c9ff2f6913a1a26fdbb
seasons986 <annalin12345@gmail.com>
2020-11-30 01:35:45 -0800

    Not done with portfolio yet, reverting src file change

--- docs/src/confetti.7a8fd7f7.js
+++ docs/src/confetti.7a8fd7f7.js
@@ -199,7 +199,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./typography-mobile.css":"src/animations/confetti/typography-mobile.css","./typography.css":"src/animations/confetti/typography.css","_css_loader":"node_modules/parcel/src/builtins/css-loader.js"}],"node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./typography-mobile.css":"src/animations/confetti/screen-mobile.css","./typography.css":"src/animations/confetti/screen.css","_css_loader":"node_modules/parcel/src/builtins/css-loader.js"}],"node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
