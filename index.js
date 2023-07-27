3ebab36478f40180ae46a191c390d30f28cea242
seasons986 <annalin12345@gmail.com>
2023-07-27 19:47:50 -0400

    got project links to work

--- index.js
+++ index.js
@@ -1,46 +1,74 @@
-let previousSelect;
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-}
-
-function onSelectProject(e) {
-  const selected = e.target.innerText;
-  console.log('selected', selected);
-  if (document.getElementById(selected)) {
-    let subelem = document.getElementById(selected);
-    subelem.style.display = "block";
-    onSelect(null, "center");
-  }
-}
-
-function onSelect(e, name="") {
-  const select = document.getElementById("select");
+window.addEventListener("load", () => {
   const left = document.getElementById("left");
   const center = document.getElementById("center");
   const right = document.getElementById("right");
-  const selected = e ? e.target.value : name;
-  console.log(e ? e.target.value : name);
-  console.log(select);
-  select.value = selected;
-  let elem;
-  switch (selected) {
-    case "projects":
-      elem = left;
-      break;
-    case "about":
-      elem = right;
-      break;
-    default:
-      elem = center;
-  }
-  if (elem) {
-    if (previousSelect) previousSelect.style.display = "none";
-    elem.style.display = "block";
-    previousSelect = elem;
-  }
-}
+  const select = document.getElementById("select");
+  const table = document.getElementById("projects-table");
+  let previousSelect = center;
 
-setTimeout(onSelect(null, "projects"),500); // remove later
+  const codeProjects = ["ai", "text", "baby", "spotify"];
+  const printProjects = ["generative", "filmotography", "badwatercolor", "creative"];
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  }
+
+  function loadProjects(projects, label) {
+    const ptr = document.createElement('tr');
+    const th = document.createElement('td');
+    th.innerText = label;
+    ptr.appendChild(th);
+    table.appendChild(ptr);
+    projects.forEach((p) => {
+      const tr = document.createElement('tr');
+      const td = document.createElement('td');
+      const a = document.createElement('a');
+      a.href = "javascript:void(0);";
+      a.onclick = onSelectProject;
+      a.innerText = p;
+      td.appendChild(a);
+      tr.appendChild(td);
+      table.appendChild(tr);
+    })
+  }
+
+  function onSelectProject(e) {
+    const selected = e.target.innerText;
+    if (document.getElementById(selected)) {
+      let subelem = document.getElementById(selected);
+      left.style.display = "none";
+      center.style.display = "block";
+      subelem.style.display = "block";
+      select.value = "center";
+    }
+  }
+
+  function onSelect(e, name="") {
+    const selected = e ? e.target.value : name;
+    if (!e) select.value = name
+    let elem;
+    switch (selected) {
+      case "projects":
+        elem = left;
+        break;
+      case "about":
+          elem = right;
+          break;
+      default:
+        elem = center;
+    }
+    if (elem) {
+      previousSelect.style.display = "none";
+      elem.style.display = "block";
+      previousSelect = elem;
+    }
+  }
+
+  select.onchange = (e) => onSelect(e);
+  onSelect(null, "projects"); // remove later
+
+  loadProjects(codeProjects, "code");
+  loadProjects(printProjects, "print");
+});
