ca84e04ada458dca3149aa3fd231215119a5ded5
seasons986 <annalin12345@gmail.com>
2023-07-27 17:59:27 -0400

    country road

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 window.addEventListener("load", () => {
+  const left = document.getElementById("left");
+  const center = document.getElementById("center");
+  const right = document.getElementById("right");
+  const select = document.getElementById("select");
+  let previousSelect = center;
+
   function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
   }
+
+  function onSelect(e) {
+    const selected = e.target.value;
+    let elem;
+    switch (selected) {
+      case "projects":
+        elem = left;
+        break;
+      case "about":
+          elem = right;
+          break;
+      default:
+        elem = center;
+    }
+    if (elem) {
+      previousSelect.style.display = "none";
+      elem.style.display = "block";
+      previousSelect = elem;
+    }
+    console.log(e.target.value);
+  }
+
+  select.onchange = (e) => onSelect(e);
 });
