ebf9b56c1f7162817e0492e711107ec6148cf143
seasons986 <annalin12345@gmail.com>
2023-07-28 01:49:36 -0400

    re-adding submodule

--- 100-days/directions/README.md
+++
@@ -1,3 +0,0 @@
-# directions
-very much inspired by paisley rekdal's work:
-https://westtrain.org/%E8%AF%97-poem-page/
--- 100-days/pull.sh
+++
@@ -1,9 +0,0 @@
-git submodule update --recursive --remote
-git add -A
-git cm "update"
-git push
-cd ../
-git add -A
-git cm "update"
-git push
-cd 100-days/
