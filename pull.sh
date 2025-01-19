6363adab21b69f897e276ef6d752f227521ae090
seasons986 <annalin12345@gmail.com>
2025-01-18 23:16:28 -0500

    cleanup and fixed pull.sh

--- pull.sh
+++ pull.sh
@@ -1,7 +1,11 @@
-git st
-git pull --rebase
-rm resume/*
-cp public/resume/* resume/
-git add -A
-git cm $1
-git push
+if [ -z "$1" ] && [ -z "$2" ]; then
+  echo "🌺 git commit message missing!"
+else
+  git st
+  git pull --rebase
+  rm resume/*
+  cp public/resume/* resume/
+  git add -A
+  git cm "$1"
+  git push
+fi
